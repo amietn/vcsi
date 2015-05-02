9035d5c6fad3e4b2008a96c25b3a95113523c8d5
amietn <amietn@foobar>
2015-05-02 16:55:49 +0200

    add setuptools script

---
+++ setup.py
@@ -0,0 +1,59 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+
+setup(
+    name='vcsi',
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version='1',
+
+    description='Create video contact sheets',
+    long_description='',
+
+    # The project's main homepage.
+    url='https://github.com/amietn/vcsi',
+
+    # Author details
+    author='Nils Amiet',
+    author_email='amietn@foobar',
+
+    # Choose your license
+    license='MIT',
+
+    # What does your project relate to?
+    keywords='video thumbnail contact sheet multimedia',
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+
+    # List run-time dependencies here.  These will be installed by pip when
+    # your project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=['numpy', 'pillow'],
+
+    # List additional groups of dependencies here (e.g. development
+    # dependencies). You can install these using the following syntax,
+    # for example:
+    # $ pip install -e .[dev,test]
+    extras_require={
+        'dev': ['check-manifest'],
+        'test': ['coverage'],
+    },
+
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # pip to create the appropriate form of executable for the target platform.
+    entry_points={
+        'console_scripts': [
+            'vcsi=vcsi:main',
+        ],
+    },
+)
---
+++ vcsi/__init__.py
@@ -0,0 +1,3 @@
+def main():
+    import vcsi.vcsi
+    vcsi.vcsi.main()
