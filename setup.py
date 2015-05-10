d661012867e5a7278e81f4a6d8f45ba14f6dcf55
amietn <amietn@foobar>
2015-05-10 20:45:18 +0200

    update package version to v3

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1',
+    version='3',
 
     description='Create video contact sheets',
     long_description='',
@@ -37,7 +37,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['numpy', 'pillow'],
+    install_requires=['numpy', 'pillow', 'jinja2'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
