198c5524647c3709e25de1b7b5067d19281494d4
amietn <amietn@foobar>
2019-01-06 15:00:44 +0100

    bump version for pypi

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='7.0.3',
+    version='7.0.4',
 
     description='Create video contact sheets, thumbnails, screenshots',
     long_description='',
